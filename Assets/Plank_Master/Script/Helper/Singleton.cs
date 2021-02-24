2be9b7ccedb84b4558ce759300ac903e9d9181fb
gogeishvili <gio.gogeishvili@gmail.com>
2021-02-24 16:28:42 +0400

    Start 24/02/2021 16:30

---
+++ Assets/Plank_Master/Script/Helper/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance { get; set; }
+    public static T instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (T)FindObjectOfType(typeof(T));
+            }
+            if (_instance == null)
+            {
+                Debug.LogError("_instance is null");
+            }
+            return _instance;
+        }
+    }
+}
